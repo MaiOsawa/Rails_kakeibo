27e794f92e24f8cacd95ccd7fbccc6aa0079d090
Mary Major <mary.major@example.comMai>
2020-09-01 14:43:49 +0000

    second commit

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,11 +1,12 @@
 class StocksController < ApplicationController
     
+    before_action :set_stock, only: [:show, :edit, :update, :destroy]
+    
     def index
         @stocks = Stock.all
     end
     
     def show
-        @stock = Stock.find(params[:id])
     end
     
     def new
@@ -16,30 +17,38 @@ class StocksController < ApplicationController
         stock_params = params.require(:stock).permit(:category, :amount)
         @stock = Stock.new(stock_params)
         if @stock.save
+            flash[:notice] = "在庫管理にデータを１件登録しました"
             redirect_to stocks_path
         else
+            flash.now[:alert] = "登録に失敗しました。"
             render :new
         end
     end
     
     def edit
-        @stock = Stock.find(params[:id])
     end
     
     def update
-        @stock = Stock.find(params[:id])
         stock_params = params.require(:stock).permit(:category, :amount)
         if @stock.update(stock_params)
+            flash[:notice] = "データを１件更新しました。"
             redirect_to stocks_path
         else
+            flash.now[:alert] = "更新に失敗しました。"
             render :edit
         end
     end
     
     def destroy
-        @stock = Stock.find(params[:id])
         @stock.destroy
+        flash[:notice] = "削除しました。"
         redirect_to stocks_path
+    end
+    
+    private
+    
+    def set_stock
+        @stock = Stock.find(params[:id])
     end
     
 end
