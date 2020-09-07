d1cf7df328ad1d4164be750c204620d2613c3cb3
Mary Major <mary.major@example.comMai>
2020-09-07 13:40:33 +0000

    最終課題3ができたのでコミット

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
-Book.create(inout: 1, category: "給料", year: 2020, month: 7, amount: 30)
-Book.create(inout: 2, category: "家賃", year: 2020, month: 7, amount: 8)
-Book.create(inout: 2, category: "食費", year: 2020, month: 7, amount: 6)
-Book.create(inout: 2, category: "光熱費・水道", year: 2020, month: 7, amount: 3)
-Book.create(inout: 2, category: "保険", year: 2020, month: 7, amount: 2)
+Book.create(inout: "収入", category: "給料", year: 2020, month: 7, amount: 30)
+Book.create(inout: "支出", category: "家賃", year: 2020, month: 7, amount: 8)
+Book.create(inout: "支出", category: "食費", year: 2020, month: 7, amount: 6)
+Book.create(inout: "支出", category: "光熱費・水道", year: 2020, month: 7, amount: 3)
+Book.create(inout: "支出", category: "保険", year: 2020, month: 7, amount: 2)
 
 Stock.create(category: "洗濯洗剤", amount: 1)
 Stock.create(category: "柔軟剤", amount: 0)
